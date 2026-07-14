2a19f6fc13449b6495a18a673d9b30693c0cae9f
havor13 <sampsonhavor43@gmail.com>
2026-07-14 04:36:25 +0000

    new

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -13,7 +13,7 @@ function Signup() {
     setSuccess("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/core/auth/register/", {
+      const response = await fetch("http://127.0.0.1:8000/api/signup/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
