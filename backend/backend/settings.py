b421378eebdbae7b26647351dc140e2acf57036e
havor13 <sampsonhavor43@gmail.com>
2026-08-02 23:43:39 +0000

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'havor_cse340_db',
-        'USER': 'havor13_cse340_db',
+        'USER': 'havor_cse340_db',
         'PASSWORD': 'pZPrZxD68vc8cnMDlyKpUrvMSDgwGpya',
         'HOST': 'dpg-d9mc2jnavr4c73f8a9e0-a.oregon-postgres.render.com',  # updated host
         'PORT': '5432',
