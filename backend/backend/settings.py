90139970b66bde4b211a203950da5ea23186ec41
havor13 <sampsonhavor43@gmail.com>
2026-08-02 22:41:05 +0000

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,11 +114,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
